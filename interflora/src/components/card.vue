a50a1e64017e0881e1425e28b4fb06226031d46c
Piyush Palod <piyushpalod@yahoo.com>
2020-10-29 00:41:07 +0100

    form validation added

--- interflora/src/components/card.vue
+++ interflora/src/components/card.vue
@@ -1,49 +1,46 @@
 <template>
-  <div>
-                <List greetingsCollection={this.greetings} />
-                <form>
-                    <div className="form-components">
-                      <textarea class="form-control" cols="50" rows="4" placeholder="Write a greeting" v-model="textBoxInput" @keyup="textChanged"></textArea>
-                        
-                        <select value={this.state.category} name="category" onChange={this.changeHandler}>
-                            <option value="choose_category" disabled>Choose category</option>
-                            <option value="category1">Valentine's Day</option>
-                            <option value="category2">Happy B'day</option>
-                            <option value="category3">Happy Anniversary</option>
-                            <option value="category4">Father's Day</option>
-                        </select>
-                    </div>
-                    <div className="form-control">
-                        <button className="button" onClick={this.submitHandler}>Add Greeting</button>
-                    </div>
-                </form>
-            </div>
+    <div class="vue-tempalte">
+      <form id="app" @submit="checkForm" action="/something" method="post">
+        <div class="form-components">
+          <textarea
+            class="form-control form-control-lg"
+            cols="50"
+            rows="4"
+            placeholder="Write a greeting"
+            v-model="textBoxInput"
+            id="textBoxInput"
+            @keyup="textChanged"
+          ></textarea>
+
+          <select  class="form-control form-control-lg" v-model="category" name="category" id="category">
+            <option value="choose_category" disabled>Choose category</option>
+            <option value="category1">Valentine's Day</option>
+            <option value="category2">Happy B'day</option>
+            <option value="category3">Happy Anniversary</option>
+            <option value="category4">Father's Day</option>
+          </select>
+        </div>
+        <div class="form-control form-control-lg">
+          <button class="btn btn-dark btn-lg btn-block" type="submit">Add Greeting</button>
+        </div>
+      </form>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  methods:{
+    checkForm:function(e) {
+      if(this.textBoxInput && this.category) return true;
+      this.errors = [];
+      if(!this.textBoxInput) this.errors.push("Message required.");
+      if(!this.category) this.errors.push("category required.");
+      e.preventDefault();
+    }
   }
-  
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- interflora/src/main.js
+++ interflora/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 createApp(App).mount('#app')
